af92817af9e7e5a9f4e07bc11780aa62e05265f4
Zach Williams <hey@zachwill.com>
2012-04-29 15:18:03 -0700

    More clean-up

--- Stamen Maps/stamen.js
+++ Stamen Maps/stamen.js
@@ -17,48 +17,44 @@ setTimeout(function(){
     var map = document.getElementById("map-main")
     map.style.width = "2048px"
     map.style.height = "1536px"
-}, 1500)
+}, 1000)
 
 setTimeout(function () {
 
-    function init() {
-        var doc = document.documentElement;
+    var doc = document.documentElement;
 
-        function getSize() {
-            return new MM.Point(2048, 1536);
-        }
-
-        var parent = document.getElementById("map-main"),
-            size = getSize(),
-            providerName = parent.getAttribute("data-provider"),
-            provider = new MM.StamenTileLayer(providerName);
-
-        function resize() {
-            try {
-                size = getSize();
-                if (main) main.setSize(size);
-            } catch (e) {}
-        }
-
-        MM.addEvent(window, "resize", resize);
-
-        // our main map
-        var main = new MM.Map(parent, provider, size, [new MM.DragHandler(), new MM.DoubleClickHandler(), new MM.TouchHandler()]);
-        parent.style.position = "absolute";
-        main.autoSize = false;
-
-        setupZoomControls(main);
-
-        // set the initial map position
-        main.setCenterZoom(new MM.Location(37.7706, -122.3782), 12);
-
-        syncMapLinks(main, [document.getElementById("home-link")], function (parts) {
-            parts.unshift(providerName);
-        });
-
-        var hasher = new MM.Hash(main);
+    function getSize() {
+        return new MM.Point(2048, 1536);
     }
 
-    init();
+    var parent = document.getElementById("map-main"),
+        size = getSize(),
+        providerName = parent.getAttribute("data-provider"),
+        provider = new MM.StamenTileLayer(providerName);
 
-}, 2000)
+    function resize() {
+        try {
+            size = getSize();
+            if (main) main.setSize(size);
+        } catch (e) {}
+    }
+
+    MM.addEvent(window, "resize", resize);
+
+    // our main map
+    var main = new MM.Map(parent, provider, size, [new MM.DragHandler(), new MM.DoubleClickHandler(), new MM.TouchHandler()]);
+    parent.style.position = "absolute";
+    main.autoSize = false;
+
+    setupZoomControls(main);
+
+    // set the initial map position
+    main.setCenterZoom(new MM.Location(37.7706, -122.3782), 12);
+
+    syncMapLinks(main, [document.getElementById("home-link")], function (parts) {
+        parts.unshift(providerName);
+    });
+
+    var hasher = new MM.Hash(main);
+
+}, 1500)
